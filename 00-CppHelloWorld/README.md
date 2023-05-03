e6f84621a7e63e8d5aa68df40952d3fcc5a61f56
Tomy-Casla <tquispehuanca@frba.utn.edu.ar>
2023-05-03 01:34:30 -0300

    Add files via upload

---
+++ 00-CppHelloWorld/README.md
@@ -0,0 +1,7 @@
+## ALGORITMOS Y ESTRUCTURAS DE DATOS
++ K1051
++ 2023
++ 164.825-1
++ Quispe
++ Tommy Jamiro
++ Visual Studio Code; Compilador MSYS2 versión 12.2.0
---
+++ 00-CppHelloWorld/hello.cpp
@@ -0,0 +1,12 @@
+/* main.cpp
+ * Hello World
+ * Quispe Tommy
+ * 20230503
+ */
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello world!" << std::endl;
+    return 0;
+}
